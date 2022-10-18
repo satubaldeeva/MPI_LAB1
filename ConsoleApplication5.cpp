0de751e2c11df7a446d26fd118ca6ee5b7879426
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-10-18 14:30:47 +0300

    Add collective programm mpi

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -2,45 +2,53 @@
 #include "mpi.h"
 #include <iostream>
 using namespace std;
-const int M = 5;
+const int M = 2;
+//C:\Users\NITRO 5\source\repos\ConsoleApplication5\Debug
+//mpiexec -n 5 MPI_LAB.exe
 
+
+int init_count() {
+	return rand()*10;
+}
 void start(const int* i, char*** n,int message) {
 
 	int ProcNum, ProcRank, RecvRank, ProcMain;
 	int Send = message;
 		MPI_Status Status;
-		
+		int secret_key;
 		MPI_Init(i, n);
 		MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 		MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-		for (int j = 0; j < M; j++) {
-			if (ProcRank == 0)
-			{
-				// Действия, выполняемые только процессом с рангом 0
-
-				for (int i = 1; i < ProcNum; i++)
-				{
-
-					MPI_Send(&Send, 1, MPI_INT, i,
-						100, MPI_COMM_WORLD);
-					printf("\nI(0) send the message to %d process: ", i);
-					MPI_Recv(&RecvRank, 1, MPI_INT, i,
-						MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-					printf("\nI got an answer from process %d", RecvRank);
+		
+		int flag = 1;
+		
+			for (int j = 0; j < M; j++) {
+				if (ProcRank == 0) {
+					secret_key = init_count();
 				}
+				MPI_Bcast(&secret_key, 1, MPI_INT, 0, MPI_COMM_WORLD);
+				
+				if (ProcRank == 0)
+				{
+					// Действия, выполняемые только процессом с рангом 0
+					printf("\nI(0) send the message to all process!");
+					for (int i = 1; i < ProcNum; i++)
+					{
+						MPI_Recv(&RecvRank, 1, MPI_INT, i,
+							MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
+						printf("\nI got an answer from %d process %d", i,RecvRank);
+					}
 
+				}
+				else { // Сообщение, отправляемое всеми процессами,
+					// кроме процесса с рангом 0
+
+					MPI_Send(&secret_key, 1, MPI_INT, 0, 100, MPI_COMM_WORLD);
+
+				}
+				cout << "\n" << j << " ends" << endl;
 			}
-			else { // Сообщение, отправляемое всеми процессами,
-				// кроме процесса с рангом 0
-				MPI_Recv(&ProcMain, 1, MPI_INT, 0,
-					MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-				printf("\nI(%d) got %3d from 0 process!", ProcRank, ProcMain);
-				MPI_Send(&ProcRank, 1, MPI_INT, 0, 100, MPI_COMM_WORLD);
-
-			}
-			cout << "\n"<< j <<" ends"<<endl;
-		}
-
+		
 		MPI_Finalize();
 	
 

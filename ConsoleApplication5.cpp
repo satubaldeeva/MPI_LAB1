f23830924e65bcc9198421da010439847ba172eb
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-10-18 16:58:07 +0300

    Lab3

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include "mpi.h"
 #include <iostream>
+#include <cassert>
 using namespace std;
 const int M = 2;
 //C:\Users\NITRO 5\source\repos\ConsoleApplication5\Debug
@@ -10,54 +11,58 @@ const int M = 2;
 int init_count() {
 	return rand()*10;
 }
+
+int* init_array(int* ar,int n) {
+	for (int i = 0; i < n; i++) {
+		ar[i] = rand();
+	}
+	return ar;
+}
 void start(const int* i, char*** n,int message) {
 
-	int ProcNum, ProcRank, RecvRank, ProcMain;
-	int Send = message;
-		MPI_Status Status;
-		int secret_key;
-		MPI_Init(i, n);
-		MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-		MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-		
-		int flag = 1;
-		
-			for (int j = 0; j < M; j++) {
-				if (ProcRank == 0) {
-					secret_key = init_count();
-				}
-				MPI_Bcast(&secret_key, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	int ProcNum, ProcRank;
+	double t1, t2, dt;
+	MPI_Init(i, n);
+	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+	int const size = ProcNum;
+	int* array= new int[size];
+	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+	t1 = MPI_Wtime();
+	int* ar_for_recv=new int[1];
+	for (int j = 0; j < M; j++) {
+		if (ProcRank == 0) {
+			array = init_array(array,size);
+		}
 				
-				if (ProcRank == 0)
-				{
-					// Действия, выполняемые только процессом с рангом 0
-					printf("\nI(0) send the message to all process!");
-					for (int i = 1; i < ProcNum; i++)
-					{
-						MPI_Recv(&RecvRank, 1, MPI_INT, i,
-							MPI_ANY_TAG, MPI_COMM_WORLD, &Status);
-						printf("\nI got an answer from %d process %d", i,RecvRank);
-					}
-
-				}
-				else { // Сообщение, отправляемое всеми процессами,
-					// кроме процесса с рангом 0
-
-					MPI_Send(&secret_key, 1, MPI_INT, 0, 100, MPI_COMM_WORLD);
-
-				}
-				cout << "\n" << j << " ends" << endl;
+		MPI_Scatter(array, 1, MPI_INT,ar_for_recv,1,MPI_INT, 0, MPI_COMM_WORLD);
+		if (ProcRank != 0) {
+			printf("Procces(%d) got this value from the main procces : %d", ProcRank, ar_for_recv[0]);
+		}
+		int* sub_avgs = NULL;
+		if (ProcRank == 0) {
+			sub_avgs = new int[size];
+			assert(sub_avgs != NULL);
+		}
+		MPI_Gather(ar_for_recv, 1, MPI_FLOAT, sub_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		if (ProcRank == 0) {
+			for(int i = 0; i < size; i++) {
+				cout << sub_avgs[i]<<" ";
 			}
-		
-		MPI_Finalize();
+		}
+		cout << "\n" << j << " ends" << endl;
+	}
+	t2 = MPI_Wtime();
+	dt = t2 - t1;
+	MPI_Finalize();
+	cout << dt << endl;
 	
 
 }
+
 
 int main(int *argc, char** argv)
 
 {
 	start(argc , &argv,12345);
-	
 	return 0;
 }

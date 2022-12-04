257ecafcca15fa2f8a7a475c37474fad378a0a8f
Zhania Satubaldeeva <zhaniasatubaldeeva@yandex.ru>
2022-12-04 19:44:43 +0300

    some changes

--- ConsoleApplication5.cpp
+++ ConsoleApplication5.cpp
@@ -1,7 +1,8 @@
-﻿#include <iostream>
+﻿#include <algorithm>
+#include <iostream>
 #include "mpi.h"
 #include <vector>
-#include <algorithm>
+
 
 using namespace std;
 
@@ -47,20 +48,11 @@ int main(int argc, char* argv[])
 {
 	const int N = 15000;
 	const int sizeGroup = 1000;
-
-
-	int ProcCount, myRank, myRank1;
-	vector<int> array;
-	
-	
+	int ProcCount, myRank,gr1_rank, gr2_rank,gr3_rank,gr4_rank, gr5_rank,inter1_rank,inter2_rank;
+	vector<int> array;	
 	int count = 0;
-
-	double dt = 0;
-
 	MPI_Status Status;
-
 	MPI_Init(&argc, &argv);
-
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcCount);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 	//Access to default group
@@ -69,17 +61,19 @@ int main(int argc, char* argv[])
 	//default_group
 	MPI_Comm_group(MPI_COMM_WORLD, &base_grp);
 	//массивы для передачи значений рангов
-	int rank1[] = { 1, 2,  3 };
-	int rank2[] = { 4, 5,  6 };
-	int rank3[] = { 7, 8,  9 };
-	int rank4[] = { 10, 11,  12 };
-	int rank5[] = { 13, 14,  15 };
-	if (myRank == 0)
-			dt -= MPI_Wtime();
-		//init vector
+	int * rank1= new int[3];
+	rank1[0]=1;
+	rank1[1] = 2;
+	rank1[2] = 3;
+	int rank2[3] = { 4, 5,  6 };
+	int rank3[3] = { 7, 8,  9 };
+	int rank4[3] = { 10, 11,  12 };
+	int rank5[3] = { 13, 14,  15 };
+	
+	//init vector
 	if (myRank == 0) {
 		for (int i = 0; i < N; i++) {
-			array[i] = rand() * 100;
+			array.push_back(rand()% 100);
 			cout << array[i] << " ";
 		}
 		
@@ -87,16 +81,40 @@ int main(int argc, char* argv[])
 	
 	//create 5 groups of proscces
 	MPI_Group_incl(base_grp, 3, rank1, &gr1);
-	MPI_Group_incl(base_grp, 3, rank2, &gr2);
-	MPI_Group_incl(base_grp, 3, rank3, &gr3);
-	MPI_Group_incl(base_grp, 3, rank4, &gr4);
-	MPI_Group_incl(base_grp, 3, rank5, &gr5);
+	MPI_Group_incl(base_grp, 3, rank1, &gr2);
+	MPI_Group_incl(base_grp, 3, rank1, &gr3);
+	MPI_Group_incl(base_grp, 3, rank1, &gr4);
+	MPI_Group_incl(base_grp, 3, rank1, &gr5);
+	MPI_Group_rank(gr1, &gr1_rank);
+	MPI_Group_rank(gr2, &gr2_rank);
+	MPI_Group_rank(gr3, &gr3_rank);
+	MPI_Group_rank(gr4, &gr4_rank);
+	MPI_Group_rank(gr5, &gr5_rank);
+	
 	//cоздание ноыого коммуникаторв с одной группе
+	
+	//MPI_Comm_split(base_grp, 0, gr1_rank, &newcomm1);
+	/*
+	if (gr2_rank == 1 || gr2_rank == 0 || gr2_rank == 2)
+		MPI_Comm_split(base_grp, 1, gr2_rank, &newcomm2);
+
+	if (gr3_rank == 1 || gr3_rank == 0 || gr3_rank == 2)
+		MPI_Comm_split(base_grp, 2, gr3_rank, &newcomm3);
+
+	if (gr4_rank == 1 || gr4_rank == 0 || gr4_rank == 2)
+		MPI_Comm_split(base_grp, 3, gr4_rank, &newcomm4);
+
+	if (gr5_rank == 1 || gr5_rank == 0 || gr5_rank == 2)
+		MPI_Comm_split(base_grp, 4, gr5_rank, &newcomm5);
+	*/
+	
+
 	MPI_Comm_create(MPI_COMM_WORLD, gr1, &newcomm1);
-	MPI_Comm_create(MPI_COMM_WORLD, gr2, &newcomm2);
+	/* 
 	MPI_Comm_create(MPI_COMM_WORLD, gr3, &newcomm3);
 	MPI_Comm_create(MPI_COMM_WORLD, gr4, &newcomm4);
 	MPI_Comm_create(MPI_COMM_WORLD, gr5, &newcomm5);
+	*/
 	
 	//wait
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -114,143 +132,143 @@ int main(int argc, char* argv[])
 	MPI_Type_commit(&type2);
 	
 	
+	
 	if (myRank == 0) {
 		//send data to newcomm1's groups
 		vector<int> v(sizeGroup);
 
 		for (int i = 0; i < 3; i++) {
-			
-			copy(array[i*sizeGroup], array[(i+1)*sizeGroup - 1], v.begin());
-			MPI_Send(&v, 1, type, i+1, 0, newcomm1);
-			v.clear();
+
+			copy(array.begin() + (i * sizeGroup), array.begin() + ((i + 1) * sizeGroup - 1), v.begin());
+			MPI_Send(&v, 1, type, i, 0, newcomm1);
+
 		}
+	}
+		/*
 		for (int i = 3; i < 6; i++) {
 
-			copy(array[i * sizeGroup], array[(i + 1) * sizeGroup - 1], v.begin());
-			MPI_Send(&v, 1, type, i + 1, 0, newcomm2);
-			v.clear();
+			copy(array.begin()+(i * sizeGroup), array.begin()+((i + 1) * sizeGroup - 1), v.begin());
+			MPI_Send(&v, 1, type, i -3, 0, newcomm2);
+			
 		}
 		for (int i = 6; i < 9; i++) {
 
-			copy(array[i * sizeGroup], array[(i + 1) * sizeGroup - 1], v.begin());
-			MPI_Send(&v, 1, type, i + 1, 0, newcomm3);
-			v.clear();
+			copy(array.begin()+(i * sizeGroup), array.begin()+((i + 1) * sizeGroup - 1), v.begin());
+			MPI_Send(&v, 1, type, i -6, 0, newcomm3);
+			
 		}
 		for (int i = 9; i < 12; i++) {
-
-			copy(array[i * sizeGroup], array[(i + 1) * sizeGroup - 1], v.begin());
-			MPI_Send(&v, 1, type, i + 1, 0, newcomm4);
-			v.clear();
+			copy(array.begin() + (i * sizeGroup), array.begin() + ((i + 1) * sizeGroup - 1), v.begin());
+			MPI_Send(&v, 1, type, i - 9, 0, newcomm4);
+			
 		}
 		for (int i = 12; i < 15; i++) {
-
-			copy(array[i * sizeGroup], array[(i + 1) * sizeGroup - 1], v.begin());
-			MPI_Send(&v, 1, type, i + 1, 0, newcomm5);
-			v.clear();
+			copy(array.begin() + (i * sizeGroup), array.begin() + ((i + 1) * sizeGroup - 1), v.begin());
+			MPI_Send(&v, 1, type, i - 12, 0, newcomm5);
+			
 		}
-
 
 	}
-	vector<int> temp;vector<int> temp13, temp123, temp23,temp13_14_15, temp13_15, temp14_15; vector<int> temp46, temp56,temp789, temp79, temp89; vector<int> temp10_12, temp11_12;
+	
+	vector<int> temp,temp13, temp123, temp23,temp13_14_15, temp13_15, temp14_15, temp46, temp56,temp789, temp79, temp89, temp10_12, temp11_12;
 	//sort and send in the group
-	for (int i = 1; i < 4; i++) {
-		
-		if (myRank == i) {
-			MPI_Recv(&temp, 1, type, 0, 0, newcomm1,&Status);
+	for (int i = 0; i < 3; i++) {
+		if (gr1_rank == i) {
+			MPI_Recv(&temp, 1, type, 0, 0, MPI_COMM_WORLD,&Status);
 			sort(temp.begin(), temp.end());
 		}
-		if (myRank == 1) {
-			MPI_Send(&temp, 1, type, myRank+2, 0, newcomm1);
+		if (gr1_rank == 0) {
+			MPI_Send(&temp, 1, type,gr1_rank+2, 0, newcomm1);
 		}
-		if (myRank == 2) {
-			MPI_Send(&temp, 1, type, myRank + 1, 0, newcomm1);
+		if (gr1_rank == 1) {
+			MPI_Send(&temp, 1, type,gr1_rank + 1, 0, newcomm1);
 		}
-		if (myRank == 3) {
+		if (gr1_rank == 2) {
 			
-			MPI_Recv(&temp13, 1, type, myRank-2, 0, newcomm1, &Status);
-			MPI_Recv(&temp23, 1, type, myRank-1, 0, newcomm1, &Status);
+			MPI_Recv(&temp13, 1, type,gr1_rank-2, 0, newcomm1, &Status);
+			MPI_Recv(&temp23, 1, type, gr1_rank-1, 0, newcomm1, &Status);
 			vector<int>temp12 = compareVector(temp13, temp23);
 			temp123 = compareVector(temp12, temp);
 		}	
 	}
-	for (int i = 4; i < 7; i++) {
+	for (int i = 0; i < 3; i++) {
 
-		if (myRank == i) {
-			MPI_Recv(&temp, 1, type, 0, 0, newcomm2, &Status);
+		if (gr2_rank== i) {
+			MPI_Recv(&temp, 1, type, 0, 0, MPI_COMM_WORLD, &Status);
 			sort(temp.begin(), temp.end());
 		}
-		if (myRank == 4) {
-			MPI_Send(&temp, 1, type, myRank + 2, 0, newcomm1);
+		if (gr2_rank == 0) {
+			MPI_Send(&temp, 1, type,gr2_rank + 2, 0, newcomm2);
 		}
-		if (myRank == 5) {
-			MPI_Send(&temp, 1, type, myRank + 1, 0, newcomm1);
+		if (gr2_rank == 1) {
+			MPI_Send(&temp, 1, type, gr2_rank + 1, 0, newcomm2);
 		}
-		if (myRank == 6) {
+		if (myRank == 2) {
 			
-			MPI_Recv(&temp46, 1, type, myRank - 2, 0, newcomm1, &Status);
-			MPI_Recv(&temp56, 1, type, myRank - 1, 0, newcomm1, &Status);
+			MPI_Recv(&temp46, 1, type, gr2_rank - 2, 0, newcomm2, &Status);
+			MPI_Recv(&temp56, 1, type,gr2_rank - 1, 0, newcomm2, &Status);
 			vector<int>temp45 = compareVector(temp46, temp56);
 			vector<int>temp456 = compareVector(temp45, temp);
 		}
 	}
-	for (int i = 7; i < 10; i++) {
+	for (int i = 0; i < 3; i++) {
 
-		if (myRank == i) {
-			MPI_Recv(&temp, 1, type, 0, 0, newcomm3, &Status);
+		if (gr3_rank == i) {
+			MPI_Recv(&temp, 1, type, 0, 0, MPI_COMM_WORLD, &Status);
 			sort(temp.begin(), temp.end());
 		}
-		if (myRank == 7) {
-			MPI_Send(&temp, 1, type, myRank + 2, 0, newcomm1);
+		if (gr3_rank == 0) {
+			MPI_Send(&temp, 1, type,gr3_rank + 2, 0, newcomm3);
 		}
-		if (myRank == 8) {
-			MPI_Send(&temp, 1, type, myRank + 1, 0, newcomm1);
+		if (gr3_rank == 1) {
+			MPI_Send(&temp, 1, type, gr3_rank + 1, 0, newcomm3);
 		}
-		if (myRank == 9) {
+		if (gr3_rank == 2) {
 			
-			MPI_Recv(&temp79, 1, type, myRank - 2, 0, newcomm1, &Status);
-			MPI_Recv(&temp89, 1, type, myRank - 1, 0, newcomm1, &Status);
+			MPI_Recv(&temp79, 1, type, gr3_rank - 2, 0, newcomm3, &Status);
+			MPI_Recv(&temp89, 1, type, gr3_rank - 1, 0, newcomm3, &Status);
 			vector<int>temp78 = compareVector(temp79, temp89);
 			temp789 = compareVector(temp78, temp);
 		}
 		
 	}
-	for (int i = 10; i < 13; i++) {
+		for (int i = 0; i < 3; i++) {
 
-		if (myRank == i) {
-			MPI_Recv(&temp, 1, type, 0, 0, newcomm4, &Status);
+		if (gr4_rank == i) {
+			MPI_Recv(&temp, 1, type, 0, 0, MPI_COMM_WORLD, &Status);
 			sort(temp.begin(), temp.end());
 		}
-		if (myRank == 10) {
-			MPI_Send(&temp, 1, type, myRank + 2, 0, newcomm1);
+		if (gr4_rank == 0) {
+			MPI_Send(&temp, 1, type,gr4_rank + 2, 0, newcomm4);
 		}
-		if (myRank == 11) {
-			MPI_Send(&temp, 1, type, myRank + 1, 0, newcomm1);
+		if (gr4_rank== 1) {
+			MPI_Send(&temp, 1, type, gr4_rank + 1, 0, newcomm4);
 		}
-		if (myRank == 12) {
+		if (gr4_rank == 2) {
 			
-			MPI_Recv(&temp10_12, 1, type, myRank - 2, 0, newcomm1, &Status);
-			MPI_Recv(&temp11_12, 1, type, myRank - 1, 0, newcomm1, &Status);
+			MPI_Recv(&temp10_12, 1, type, gr4_rank - 2, 0, newcomm4, &Status);
+			MPI_Recv(&temp11_12, 1, type,gr4_rank - 1, 0, newcomm4, &Status);
 			vector<int>temp10_11 = compareVector(temp11_12, temp10_12);
 			vector<int>temp10_11_12 = compareVector(temp10_11, temp);
 		}
 		
 	}
-	for (int i = 13; i < 16; i++) {
+	for (int i = 0; i < 3; i++) {
 
-		if (myRank == i) {
-			MPI_Recv(&temp, 1, type, 0, 0, newcomm5, &Status);
+		if (gr5_rank == i) {
+			MPI_Recv(&temp, 1, type, 0, 0,MPI_COMM_WORLD, &Status);
 			sort(temp.begin(), temp.end());
 		}
-		if (myRank == 13) {
-			MPI_Send(&temp, 1, type, myRank + 2, 0, newcomm1);
+		if (gr5_rank == 0) {
+			MPI_Send(&temp, 1, type, myRank + 2, 0, newcomm5);
 		}
-		if (myRank == 14) {
-			MPI_Send(&temp, 1, type, myRank + 1, 0, newcomm1);
+		if (gr5_rank == 1) {
+			MPI_Send(&temp, 1, type, myRank + 1, 0, newcomm5);
 		}
-		if (myRank == 15) {
+		if (gr5_rank == 2) {
 			
-			MPI_Recv(&temp13_15, 1, type, myRank - 2, 0, newcomm1, &Status);
-			MPI_Recv(&temp14_15, 1, type, myRank - 1, 0, newcomm1, &Status);
+			MPI_Recv(&temp13_15, 1, type, gr5_rank - 2, 0, newcomm5, &Status);
+			MPI_Recv(&temp14_15, 1, type, gr5_rank - 1, 0, newcomm5, &Status);
 			vector<int>temp13_14 = compareVector(temp13_15, temp14_15);
 			temp13_14_15 = compareVector(temp13_14, temp);
 		}
@@ -264,12 +282,16 @@ int main(int argc, char* argv[])
 	MPI_Barrier(newcomm3);
 	MPI_Barrier(newcomm4);
 	MPI_Barrier(newcomm5);
-
-	MPI_Intercomm_create(newcomm1, 3, newcomm2,6, 0, &inter_comm12);
-	MPI_Intercomm_create(newcomm1, 3, newcomm2, 6, 0, &inter_comm34);
+	
+	MPI_Intercomm_create(newcomm1, 2, newcomm2,2, 0, &inter_comm12);
+	MPI_Comm_rank(inter_comm12, &inter1_rank);
+	cout << inter1_rank << endl;
+	MPI_Intercomm_create(newcomm3, 2, newcomm4, 2, 0, &inter_comm34);
+	MPI_Comm_rank(inter_comm34, &inter2_rank);
+	cout << inter2_rank << endl;
 	vector<int> dataFromGroup1,dataFromGroup3;
 	vector<int> resGroup12,resGroup34;
-
+	/*
 	//TO 3-6
 	if (myRank == 3) {
 		MPI_Send(&temp123, 1,type2, 6, 0, inter_comm12);
@@ -314,8 +336,13 @@ int main(int argc, char* argv[])
 	
 	
 
-	
-
+	*/
+	MPI_Group_free(&base_grp);
+	MPI_Group_free(&gr2);
+	MPI_Group_free(&gr3);
+	MPI_Group_free(&gr1);
+	MPI_Group_free(&gr4);
+	MPI_Group_free(&gr5);
 	MPI_Type_free(&type);
 	MPI_Type_free(&type1);
 	MPI_Type_free(&type2);
